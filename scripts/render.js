cc369cbb619941c11e1bcf2aa02b55bb0a21110d
renki <renki@cs.uchicago.edu>
2020-05-26 01:29:38 -0500

    rendering util functions; canvas sections

--- scripts/render.js
+++ scripts/render.js
@@ -8,8 +8,37 @@
  */
 function renderGameState(canvas, gameState, players, localContext) {
     var context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.font = '10px Courier'
-    context.fillStyle = 'black';
-    context.fillText(gameState, 5, 15);
+
+    // Coordinate variables
+    max_x = canvas.width
+    max_y = canvas.height
+    board_start_x = max_x/4
+    hand_start_y = 3*max_y/4
+
+    // Draw functions for each section
+    const draw_panel = draw_function(context, 0, 0, board_start_x, max_y)
+    const draw_board =  draw_function(context, board_start_x, 0, max_x, hand_start_y)
+    const draw_hand = draw_function(context, board_start_x, hand_start_y, max_x, max_y)
+    
+    // Drawing
+    context.clearRect(0, 0, max_x, max_y);
+
+    context.fillStyle = 'rgba(0, 0, 100, 0.1)'
+    draw_panel('fillRect', [0, 0, 100, 100])
+    context.fillStyle = 'rgba(0, 100, 0, 0.1)'
+    draw_board('fillRect', [0, 0, 100, 100])
+    context.fillStyle = 'rgba(100, 0, 0, 0.1)'
+    draw_hand('fillRect', [0, 0, 100, 100])
+
+    context.font = '20px Courier'
+    context.fillStyle = 'black'
+    draw_board('fillText', ['BOARD', 40, 10])
+    draw_hand('fillText', ['HAND', 40, 40])
+    draw_panel('fillText', ['METADATA', 25, 10])
+    draw_panel('fillText', ['SCORE', 30, 40])
+    draw_panel('fillText', ['LAST TRICK', 15, 60])
+
+    context.font = '12px Courier'
+    context.fillStyle = 'black'
+    draw_panel('fillText', [`gameState: ${gameState}`, 5, 15], resize=false)
 }
--- scripts/util.js
+++ scripts/util.js
@@ -13,3 +13,67 @@ function isValidJson(string) {
         return false;
     }
 }
+
+/**
+ * 
+ * Execute a canvas function, converting relative coordinates to absolute coordinates on the canvas.
+ * 
+ * @param {context} context canvas context
+ * @param {String} f canvas function to call
+ * @param {Number} xstart x-coordinate of canvas area start
+ * @param {Number} ystart y-coordinate of canvas area start
+ * @param {Number} xend x-coordinate of canvas area end
+ * @param {Number} yend y-coordinate of canvas area end
+ * @param {Number} base_x max x coordinate in base terms
+ * @param {Number} base_y max y coordinate in base terms
+ * @param {list} args args to pass context.f function, which will be translated + resized
+ * @param {boolean} resize resize coordinates or not
+ * Mode of resizing is inferred from length of args:
+ *  2: x, y
+ *  3: arg (no resize), x, y
+ *  4: x, y, width, height
+ *  5: arg (no resize), x, y, width, height
+ */
+function draw_resize(context, f, xstart, ystart, xend, yend, base_x = 100, base_y = 100, args, resize) {
+    if (args.length < 2){
+        throw `Error: too few arguments given to draw_resize. f: ${f} args: ${args}`
+    }
+    if (args.length > 5){
+        throw `Error: too many arguments given to draw_resize. f: ${f} args: ${args}`
+    }
+    i=0
+    // If there are an odd number of args, the first arg is not resized
+    if (args.length % 2 == 1){
+        i=1
+    }
+    if (resize) {
+        factor_x = (Math.floor(xend) - Math.floor(xstart))/base_x
+        factor_y = (Math.floor(yend) - Math.floor(ystart))/base_y
+        args[i] = args[i]*factor_x
+        args[i+1] = args[i+1]*factor_y
+    }
+    args[i] = xstart + args[i]
+    args[i+1] = ystart + args[i+1]
+    // If there are >3 args, need to resize 'width' and 'height' parameters
+    if (args.length > 3 && resize){
+        args[i+2] = args[i+2]*factor_x
+        args[i+3] = args[i+3]*factor_y
+    }
+    context[f].apply(context, args)
+}
+
+/**
+ * 
+ * Create a draw_resize function with set canvas, start/end area, base coordinates
+ * 
+ * @param {context} context canvas context
+ * @param {Number} xstart x-coordinate of canvas area start
+ * @param {Number} ystart y-coordinate of canvas area start
+ * @param {Number} xend x-coordinate of canvas area end
+ * @param {Number} yend y-coordinate of canvas area end
+ * @param {*} base_x 
+ * @param {*} base_y 
+ */
+function draw_function(context, xstart, ystart, xend, yend, base_x = 100, base_y = 100){
+    return (f, args, resize=true) => draw_resize(context, f, xstart, ystart, xend, yend, base_x, base_y, args, resize)
+}
