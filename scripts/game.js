0b0375770228f6ba63adbf63463c903299de08a3
John Zhao <johnzhao96@uchicago.edu>
2020-06-13 01:55:37 -0700

    Introduce model definitions for GameState, Move, and Card

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,184 @@
-function GameState() {
-    /* TODO: define GameState class here */ 
+/*
+///////////////
+// GameState //
+///////////////
+
+{
+    // position of dealer, tracks who starts play each round
+    dealer: {Number},
+
+    // current points of each team
+    // teamPoints[0] = Team 0's points (positions 0,2)
+    // teamPoints[1] = Team 1's points (positions 1,3)
+    teamPoints: [{Number}]
+
+    // round-specific state
+    roundState: {
+        // position of player to make next move
+        actionPosition: {Number},
+
+        // List of cards held by each player
+        cards: {
+            "0": [{Card}],
+            "1": [{Card}],
+            "2": [{Card}],
+            "3": [{Card}]
+        },
+
+        // phase-specific state information
+        phaseState: {
+            // name of phase, must be one of the following:
+            //     "BET_PHASE_0"
+            //     "BET_PHASE_1"
+            //     "BET_PHASE_2"
+            //     "PLAY_PHASE"
+            phaseName: {String},
+            
+            /////////////////////////vvv
+            // If BET_PHASE_0
+
+            // List of positions of players who decide to blind nil
+            blindNils: [{Number}],
+            /////////////////////////^^^
+
+            /////////////////////////vvv
+            // If BET_PHASE_1
+
+            // List of blind nil positions (carried over from BET_PHASE_0)
+            blindNils: [{Number}],
+
+            // List of positions of players who have passed from betting
+            passed: [{Number}],
+
+            // Current maximum bet
+            maxBet: {
+                // position of player who made this bet
+                position: {Number},
+
+                // number of tricks bet
+                trickCount: {Number},
+
+                // proposed trump suit, must be one of "c", "d", "h", "s"
+                trumpSuit: {String}
+            },
+            /////////////////////////^^^
+
+            /////////////////////////vvv
+            // If BET_PHASE_2
+
+            // chosen trump suit, must be one of "c", "d", "h", "s"
+            trumpSuit: {String},
+
+            // map of player position to bets their bet trick counts
+            bets: {
+                "0": {Number},
+                "1": {Number},
+                "2": {Number},
+                "3": {Number}
+            },
+            /////////////////////////^^^
+
+            /////////////////////////vvv
+            // If PLAY_PHASE
+
+            // chosen trump suit (carried over from BET_PHASE_2)
+            trumpSuit: {String},
+
+            // total number of tricks each team needs to make
+            // tricksToMake[0] = number of tricks Team 0 needs to make
+            // tricksToMake[1] = number of tricks Team 1 needs to make
+            tricksToMake: [{Number}],
+
+            // cards in current trick
+            currentTrick: {
+                "0": {Card},
+                "1": {Card},
+                "2": {Card},
+                "3": {Card}
+            },
+
+            // starting suit of current trick, must be one of "c", "d", "h", "s"
+            startingSuit: {String}
+
+            // number of tricks taken by each team
+            // tricksTaken[0] = number of tricks taken by Team 0
+            // tricksTaken[1] = number of tricks taken by Team 1
+            tricksTaken: [{Number}]
+            /////////////////////////^^^
+        }
+    }
 }
+*/
+
+/*
+//////////
+// Move //
+//////////
+
+{
+    // position of player making this move
+    actionPosition: {Number},
+
+    // name of phase, must be one of the following:
+    //     "BET_PHASE_0"
+    //     "BET_PHASE_1"
+    //     "BET_PHASE_2"
+    //     "PLAY_PHASE"
+    phaseName: {String},
+
+    /////////////////////////vvv
+    // If BET_PHASE_0
+
+    // True if the move-maker wants to go blind nil, false otherwise
+    goBlindNil: {Boolean},
+    /////////////////////////^^^
+
+    /////////////////////////vvv
+    // If BET_PHASE_1
+
+    // True if the move-maker wants to pass, false otherwise
+    goPass: {Boolean},
+
+    // number of tricks to bet (ignored if goPass == true)
+    trickCount: {Number},
+
+    // trump suit, must be one of "c", "d", "h", "s" (ignored if goPass == true)
+    trumpSuit: {String},
+    /////////////////////////^^^
+
+    /////////////////////////vvv
+    // If BET_PHASE_2
+
+    // number of tricks to bet
+    trickCount: {Number},
+    /////////////////////////^^^
+
+    /////////////////////////vvv
+    // If PLAY_PHASE
+
+    // card to play
+    card: {Card}
+    /////////////////////////^^^
+}
+*/
+
+/*
+//////////
+// Card //
+//////////
+
+{
+    // card number
+    // 11 = Jack
+    // 12 = Queen
+    // 13 = King
+    // 14 = Ace
+    number: {Number},
+
+    // card suit, must be one of "c", "d", "h", "s"
+    suit: {String}
+}
+*/
 
 function makeMoveIfValid(gameState, move) {
     if (isValidMove(gameState, move)) {
